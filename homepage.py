3f5d887f8312da7d26231d492fc704afac704e7d
arianazinggg <110186183+arianazinggg@users.noreply.github.com>
2024-03-07 20:48:25 +0800

    title

---
+++ homepage.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="SAMAHAN Directory", 
+                    page_icon=":book:",
+                    layout="wide",
+                    initial_sidebar_state="expanded",
+)
+
+st.title("SAMAHAN Directory")
