cc1b90c5f98ac076e90e1bbe3c3689daaba5654a
arianazinggg <110186183+arianazinggg@users.noreply.github.com>
2024-03-08 11:15:51 +0800

    rawr

--- homepage.py
+++ homepage.py
@@ -28,13 +28,10 @@ tile_data = [
     ]
 
 for i, (name, color) in enumerate(tile_data):
-    col = [
-        row1[i] if i < 2 else row2[i-2]
+    col = row1[i] if i < 2 else row2[i-2]
     with col:
-        if st.button(label=name, key=name):
+        if st.markdown(f'<div style="background-color: {color}; height: 150px; padding: 20px; margin: 10px; text-align: center; font-family: Arial, sans-serif; font-weight: bold; font-size: 20px; border-radius: 10px;">{name}</div>', unsafe_allow_html=True)
             navigate_to_page(name)
-        st.markdown(f'<div style="background-color: {color}; height: 150px; padding: 20px; margin: 10px; text-align: center; font-family: Arial, sans-serif; font-weight: bold; font-size: 20px; border-radius: 10px;">{name}</div>', unsafe_allow_html=True)
-    ]
     
 def navigate_to_page(name, color):
     st.title(f"You clicked {name}")
