d0e13860e4027723278c1e1bda388aab932b4b59
arianazinggg <110186183+arianazinggg@users.noreply.github.com>
2024-03-08 22:23:27 +0800

    hmmm

--- homepage.py
+++ homepage.py
@@ -21,7 +21,7 @@ st.download_button(
 
 # Define the tabs
 tabs = ["Home", "Reservation of Venues", "Letter Templates", "School Administration", "Ateneo Map"]
-selected_tab_index = st.selectbox("Select a tab:", range(len(tab_names)), format_func=lambda x: tab_names[x])
+selected_tab_index = st.selectbox("Select a tab:", range(len(tabs)), format_func=lambda x: tabs[x])
 
 # Display content based on the selected tab
 if selected_tab == "Home":
