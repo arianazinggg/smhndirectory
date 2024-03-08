9693109a4e784a2c113376b59179c1018a765712
arianazinggg <110186183+arianazinggg@users.noreply.github.com>
2024-03-08 10:57:37 +0800

    name

--- homepage.py
+++ homepage.py
@@ -31,8 +31,8 @@ for i, (name, color) in enumerate(tile_data):
         col = row1[i] if i < 2 else row2[i-2]
         with col:
             st.markdown(f'<div style="background-color: {color}; height: 150px; padding: 20px; margin: 10px; text-align: center; font-family: Arial, sans-serif; font-weight: bold; font-size: 20px; border-radius: 10px;">{name}</div>', unsafe_allow_html=True)
-if st.button(Reservation_of_Venues):
-            navigate_to_page(Reservation_of_Venues)
+if st.button(name):
+            navigate_to_page(name)
 def navigate_to_page(Reservation_of_Venues):
     st.title(f"{Reservation_of_Venues}")
     st.write("This is the content of the new page.")
