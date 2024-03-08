250992c7ce094e15f8f6bff4bcf2472aca079afd
arianazinggg <110186183+arianazinggg@users.noreply.github.com>
2024-03-08 11:09:01 +0800

    label

--- homepage.py
+++ homepage.py
@@ -30,7 +30,7 @@ tile_data = [
 for i, (name, color) in enumerate(tile_data):
         col = row1[i] if i < 2 else row2[i-2]
         with col:
-            if st.button(name, key=name):
+            if st.button(label=name, key=name):
                 navigate_to_page(name)
             st.markdown(f'<div style="background-color: {color}; height: 150px; padding: 20px; margin: 10px; text-align: center; font-family: Arial, sans-serif; font-weight: bold; font-size: 20px; border-radius: 10px;">{name}</div>', unsafe_allow_html=True)
             
