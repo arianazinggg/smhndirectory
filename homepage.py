5792b119cbd05a182dc3c6cfca275cda47552bca
arianazinggg <110186183+arianazinggg@users.noreply.github.com>
2024-03-08 21:20:24 +0800

    hehe

--- homepage.py
+++ homepage.py
@@ -25,7 +25,7 @@ with tab1:
 
 with tab2:
     st.header('📆Reservation of Venues')
-    st.markdown('<div style="display: inline-block; padding: 10px;border-radius: 10px;text-align: center; font-family: Helvetica; font-size: 20px;color: white; background-color: #124076;margin-top: 8px;">STEP 1</div>', unsafe_allow_html=True)
+    st.markdown('<div style="display: inline-block; padding: 10px;border-radius: 10px;text-align: center; font-family: Helvetica; font-size: 20px;color: white; background-color: #124076;margin-top: 8px; margin-bottom: 8px;">STEP 1</div>', unsafe_allow_html=True)
 
 st.markdown("""
     Submit the Concept Paper to the Office of the Student Affairs
