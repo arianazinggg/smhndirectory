c7fcc26ac3f1e6227910b697f15d1bc38d5e0477
arianazinggg <110186183+arianazinggg@users.noreply.github.com>
2024-03-08 10:21:22 +0800

    container

--- homepage.py
+++ homepage.py
@@ -17,36 +17,9 @@ st.download_button(
         key='download-pdf'
     )
 
-tab1, tab2= st.tabs(["Homepage","Contact Us" ])
+row1 = st.columns(3)
+row2 = st.columns(3)
 
-    
-    # CSS for styling the tiles
-css = """
-    <style>
-    .tile {
-        background-color: #f4f4f4;
-        border-radius: 5px;
-        padding: 20px;
-        margin: 10px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    }
-    </style>
-    """
-st.markdown(css, unsafe_allow_html=True)
-    
-    # Tiles
-st.write('<div class="tile">Reserving of Venues<br>This is the content of Tile 1</div>', unsafe_allow_html=True)
-st.write('<div class="tile">Making Letters<br>This is the content of Tile 2</div>', unsafe_allow_html=True)
-st.write('<div class="tile">School Admin<br>This is the content of Tile 3</div>', unsafe_allow_html=True)
-
-col1, col2 = st.columns(2)
-
-    # Place elements in the first column
-with col1:
-        st.write("This is inside the first column.")
-        st.write("You can put multiple elements inside a column.")
-
-    # Place elements in the second column
-with col2:
-        st.write("This is inside the second column.")
-        st.write("You can put multiple elements inside a column.")
+for col in row1 + row2:
+    tile = col.container(height=120)
+    tile.title(":book:")
